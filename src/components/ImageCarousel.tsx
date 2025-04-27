49ab70f0e05fc800baa90a8b40b273973d528f7d
simsung1 <sungmin.sim@outlook.com>
2025-04-27 11:06:22 -0400

    refactor: create separate data file for carousel images

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -4,6 +4,18 @@ import Slider from "react-slick";
 import Image from "next/image";
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline'
 
+interface CarouselImage {
+    src: string;
+    alt: string;
+    styling?: string;
+    bgColor?: string;
+    divContent?: React.ReactNode;
+}
+
+interface ImageCarouselProps {
+    images: CarouselImage[];
+}
+
 const NextArrow = (props: { onClick?: () => void }) => {
     const { onClick } = props;
     return (
@@ -28,7 +40,7 @@ const PrevArrow = (props: { onClick?: () => void }) => {
     );
 };
 
-const ImageCarousel = () => {
+const ImageCarousel: React.FC<ImageCarouselProps> = ({ images }) => {
     const settings = {
         dots: true,
         infinite: true,
@@ -41,13 +53,6 @@ const ImageCarousel = () => {
         nextArrow: <NextArrow />,
         prevArrow: <PrevArrow />,
     };
-
-    const images = [
-        { src: "/images/wsib-edit.jpg", alt: "WSIB Logo with Summer Student text", styling: "object-contain object-bottom px-10 pb-5 md:object-right", bgColor: 'bg-[#2A3452]', divContent: <div className="text-white text-center z-1 absolute top-15 md:top-1/3 md:w-3/5 lg:w-1/2 md:left-5"><div className="font-sharpsans text-3xl ">Summer Student Position Starting in MAY</div><div className="text-xl mt-2">IT Service Delivery</div></div>},
-        { src: "/images/fanshawecampus.jpg", alt: "Fanshawe College T building", styling: "object-cover brightness-50", divContent: <div className="text-white text-center z-1 absolute top-15 md:top-1/3 md:w-3/5 lg:w-1/2 lg:left-5 flex flex-col items-center"><div className="font-sharpsans text-3xl ">First Year of CPA3 Program <span className="text-red-600">Completed</span></div><div className="text-xl mt-2 text-white">Second Year Starts Sept 2025</div><a href="#" target="_blank" rel="noopener noreferrer" className="mt-10 md:mt-3 w-fit rounded-full bg-white text-black p-4">Learn More</a></div>},
-        { src: "/images/fcvlogo.jpg", alt: "Forest City Vape Logo", styling: "object-contain object-bottom px-10 pb-5 md:object-right", bgColor: 'bg-[#5B7E55]', divContent: <div className="text-white text-center z-1 absolute top-1/3 md:w-3/5 lg:w-1/2 md:left-10 flex flex-col items-center"><div className="font-sharpsans text-3xl ">Celebrating One Year with FCV</div><div className="text-xl mt-2">Thank you to the team for the amazing support!</div><a href="#" target="_blank" rel="noopener noreferrer" className="mt-5 md:mt-3 w-fit rounded-full bg-black text-white p-4">Learn More</a></div> },
-        { src: "/images/vue.png", alt: "Image 4", bgColor: 'bg-green-200' },
-    ];
 
     return (
         <div className="relative w-full max-w-[1440px] h-[70vh] mb-10 max-h-[640px] mx-auto md:h-[30vh] lg:h-[70vh]">
---
+++ src/data/carouselImages.tsx
@@ -0,0 +1,40 @@
+import { ReactNode } from "react";
+
+interface CarouselImage {
+    src: string;
+    alt: string;
+    styling?: string;
+    bgColor?: string;
+    divContent?: ReactNode;
+}
+
+export const images: CarouselImage[] = [
+    {
+        src: "/images/wsib-edit.jpg",
+        alt: "WSIB Logo with Summer Student text",
+        styling: "object-contain object-bottom px-10 pb-5 md:object-right",
+        bgColor: 'bg-[#2A3452]',
+        divContent: <div className="text-white text-center z-1 absolute top-15 md:top-1/3 md:w-3/5 lg:w-1/2 md:left-15 md:text-left"><div className="font-sharpsans text-3xl ">Summer Student Position Starting in MAY</div><div className="text-xl mt-2">IT Service Delivery</div></div>
+    },
+    {
+        src: "/images/fanshawecampus.jpg",
+        alt: "Fanshawe College T building",
+        styling: "object-cover brightness-50",
+        divContent: <div className="text-white text-center z-1 absolute top-15 md:top-1/3 md:w-3/5 lg:w-1/2 md:left-15 flex flex-col items-center md:block md:text-left"><div className="font-sharpsans text-3xl ">First Year of CPA3 Program <span className="text-red-600">Completed</span></div><div className="text-xl mt-2 text-white">Second Year Starts Sept 2025</div><a href="https://www.fanshawec.ca/programs/cpa3-computer-programming-and-analysis-co-op/courses-next" target="_blank" rel="noopener noreferrer" className="mt-5 lg:mt-10 w-fit rounded-full bg-white text-black px-4 py-2 inline-block">Learn More</a></div>
+    },
+    {
+        src: "/images/fcvlogo.jpg",
+        alt: "Forest City Vape Logo",
+        styling: "object-contain object-bottom px-10 pb-5 md:object-right",
+        bgColor: 'bg-[#5B7E55]',
+        divContent: <div className="text-white text-center z-1 absolute top-1/3 md:w-3/5 lg:w-1/2 md:text-left md:left-15 flex flex-col items-center md:block"><div className="font-sharpsans text-3xl ">Celebrating One Year with FCV</div><div className="text-xl mt-2">Thank you to the team for the amazing support!</div><a href="https://www.forestcityvape.com/" target="_blank" rel="noopener noreferrer" className="mt-5 lg:mt-10 w-fit rounded-full bg-black text-white px-4 py-2 inline-block">Learn More</a></div> 
+    },
+    {
+        src: "/images/homescreenshot.png",
+        alt: "Screenshot of portfolio page in ipad mini view",
+        styling: "object-contain object-bottom px-10 pb-5 md:object-right pt-1/2 brightness-50",
+        bgColor: 'bg-[#3C3C3C]',
+        divContent: <div className="text-white text-center z-1 absolute px-2 top-15 md:top-1/3 md:w-3/5 lg:w-1/2 md:left-15 md:text-left"><div className="font-sharpsans text-3xl ">Portfolio Site Launched 🎉</div><div className="text-xl mt-2">Here&apos;s to more successful prjects in the future 🥂</div></div>
+    
+    },
+];
