34ebf06f75d0c2bb09b24dc98a073db6bb636887
simsung1 <sungmin.sim@outlook.com>
2025-05-02 15:42:39 -0400

    feat: create info3134 java page

---
+++ src/app/projects/info3134/page.tsx
@@ -0,0 +1,24 @@
+import ProjectLayout from "@/components/ProjectLayout";
+
+export default function JavaPage() {
+    return (
+        <ProjectLayout
+            images={[
+                "/images/java-1.png",
+                "/images/java-2.png",
+                "/images/java-3.png"
+            ]}
+            title="Fractions Calculator, Swing GUI"
+            description="A major project from the Java course where we implemented a set of classes 
+                        based on a provided UML diagram. Using core object-oriented programming 
+                        principles (encapsulation, inheritance, polymorphism), we structured the codebase. 
+                        Integrated action listeners and event handlers to build a fully functional GUI 
+                        using Java’s Swing library."
+            techStack={["Java"]}
+            goals={[
+                "Practiced overriding the built-in toString() method in custom objects to produce formatted output",
+                "Explored multiple JFrame layout options within the Swing library to make the program visually appealing",
+            ]}
+        />
+    );
+}
